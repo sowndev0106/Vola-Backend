0d9a3a9c2fc6607e7a05cdb76c5718e338a63068
Nguyễn Thanh Sơn <70257816+nguyenthanhson162001@users.noreply.github.com>
2022-11-26 15:00:48 +0700

    Update IdValidate.js

--- build/util/validate/IdValidate.js
+++ build/util/validate/IdValidate.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = (id) => {
     const regexMongoId = /^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i;
-    if (!id || !regexMongoId.test(id))
+    if (!id)
         throw new Error(`mongo id is invalid`);
     return id;
 };
